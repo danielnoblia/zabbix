ee3c7f09bbe0b18f2c2725fcaf2e9c8fa5a44a1b
Daniel Noblia <78825802+danielnoblia@users.noreply.github.com>
2023-09-01 17:07:56 +0200

    Update certmon_get_certs.ps1 $ca_hostname
    Glömde byta ut variablen för $ca_hostname

--- templates/adcs/certmon_get_certs.ps1
+++ templates/adcs/certmon_get_certs.ps1
@@ -3,21 +3,21 @@ $ca_hostname=$args[0]
 $template=$args[1]
 $start = (Get-Date).AddDays(-7)
 
-$certcount = Get-IssuedRequest -CertificationAuthority srvoirca -Filter "NotAfter -ge $start", "CertificateTemplate -eq $template" |
+$certcount = Get-IssuedRequest -CertificationAuthority $ca_hostname -Filter "NotAfter -ge $start", "CertificateTemplate -eq $template" |
     Group-Object -Property Request.RequesterName |
     ForEach-Object { $_.Group | Sort-Object NotAfter -Descending | Select-Object -First 1 }
 
 
 If ($certcount.Count -Eq 1)
 {
-  ('[' + (Get-IssuedRequest -CertificationAuthority srvoirca -Filter "NotAfter -ge $start", "CertificateTemplate -eq $template" |
+  ('[' + (Get-IssuedRequest -CertificationAuthority $ca_hostname -Filter "NotAfter -ge $start", "CertificateTemplate -eq $template" |
     Group-Object -Property Request.RequesterName |
     ForEach-Object { $_.Group | Sort-Object NotAfter -Descending | Select-Object -First 1 } |
     Select-Object -Property RequestID, Request.RequesterName, CommonName, NotAfter, CertificateTemplateOid |
     ConvertTo-Json -Compress) + ']')
 
 } else {
-    Get-IssuedRequest -CertificationAuthority srvoirca -Filter "NotAfter -ge $start", "CertificateTemplate -eq $template" |
+    Get-IssuedRequest -CertificationAuthority $ca_hostname -Filter "NotAfter -ge $start", "CertificateTemplate -eq $template" |
     Group-Object -Property Request.RequesterName |
     ForEach-Object { $_.Group | Sort-Object NotAfter -Descending | Select-Object -First 1 } |
     Select-Object -Property RequestID, Request.RequesterName, CommonName, NotAfter, CertificateTemplateOid |
